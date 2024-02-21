b745e6a1c3af51f659b0c774cdc7f7ae72bff506
Elm2006 <isel06002@utb.vaxjo.se>
2024-02-21 10:52:09 +0100

    Add project files.

---
+++ inlämning6.8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ett heltal");
+        int svar = int.Parse(Console.ReadLine());
+        Console.WriteLine(Primtal(svar)+" tal");
+    }
+    static int Primtal(int svar)
+    {
+        int gånger = 0;
+        for(int i = svar-1; i>0;  i--)
+        {
+            if(i%2 != 0)
+            {
+                gånger++;
+            }
+        }
+        return gånger;
+    }
+}
